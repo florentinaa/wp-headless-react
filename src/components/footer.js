d7d40f1f502258a60113269a998027b5b585ca63
Florentina Sultan <florentina@florentinasultan.co.uk>
2018-10-11 11:10:05 +0100

    instalWP REST API Sidebars plugin to render sidebars API and pull the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
+    constructor(){
+        super();
+        this.state = {
+            footer: []
+        }
+    }
+    componentDidMount() {
+        let footerUrl = 'https://flore.local/wp-json/wp-rest-api-sidebars/v1/sidebars/footer';
+        fetch(footerUrl)
+        .then(response=>response.json())
+        .then(response => {
+            this.setState({
+                footer: response
+            })
+        })
+    }
     render() {
+        let sidebar = this.state.footer;
         return(
         <footer className="footer">
-            <div>Website Powered by Florentina</div>
+            <div dangerouslySetInnerHTML  = {{__html:sidebar.rendered}} />
         </footer>
         )
     }
--- wordpress/wp-content/themes/restapi/functions.php
+++ wordpress/wp-content/themes/restapi/functions.php
@@ -6,5 +6,3 @@ if (!defined('ABSPATH')) {
 
 require_once('lib/sidebars.php');
 require_once('endpoints/header.php');
-// require_once('endpoints/sidebar.php');
-// require_once('endpoints/widget.php');
